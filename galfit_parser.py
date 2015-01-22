670af710d868e29fda1c7a44b205e45afb12ce22
Alex Hagen <mr.alex.hagen@gmail.com>
2015-01-22 12:19:15 -0500

    added GalfitComponent class and finished GalfitResults class. this successfully works on a galfit output file with one component. it should work for any output file.

--- galfit_parser.py
+++ galfit_parser.py
@@ -4,6 +4,36 @@
 
 from astropy.io import fits
 
+class GalfitComponent(object):
+    """
+    stores results from one component of the fit
+    """
+    def __init__(self,galfitheader,component_number):
+        """
+        takes in the fits header from HDU 3 (the galfit model) from a galfit output file
+        and the component number to extract
+        """
+        #checks
+        assert component_number > 0
+        assert "COMP_" + str(component_number) in galfitheader
+
+        self.component_type = galfitheader["COMP_" + str(component_number)]
+        self.component_number = component_number
+        headerkeys = [i for i in galfitheader.keys()]
+        comp_params = []
+        for i in headerkeys:
+            if str(component_number) + '_' in i:
+                comp_params.append(i)
+        for param in comp_params:
+            val = galfitheader[param]
+            #we know that val is a string formatted as 'result +/- uncertainty'
+            val = val.split()
+            paramsplit = param.split('_')
+            setattr(self,paramsplit[1].lower(),float(val[0]))
+            setattr(self,paramsplit[1].lower() + '_err',float(val[2]))
+            
+            
+
 class GalfitResults(object):
     """
     This class stores galfit results information
@@ -20,14 +50,13 @@ class GalfitResults(object):
         galfitmodel = hdulist[2]
         galfitheader = galfitmodel.header
         galfit_in_comments = False
-        for i in galfitheader['COMMENTS']:
+        for i in galfitheader['COMMENT']:
             galfit_in_comments = galfit_in_comments or "GALFIT" in i
         assert True == galfit_in_comments
         assert "COMP_1" in galfitheader
         #now we've convinced ourselves that this is probably a galfit file
         
         self.galfit_fits_file = galfit_fits_file
-        self.fitsheader = galfitheader
         #read in the input parameters
         self.input_initfile = galfitheader['INITFILE']
         self.input_datain = galfitheader["DATAIN"]
@@ -53,8 +82,9 @@ class GalfitResults(object):
             else:
                 break
         self.num_components = num_components
-        
-        headerkeys = [i for i in galfitheader.keys()]
-        
-        
+
+        for i in range(1, self.num_components + 1):
+            setattr(self,"component_" + str(i),GalfitComponent(galfitheader,i))
+
+        hdulist.close()
         

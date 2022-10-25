a17772112dfc0e3cf8fa9f378d23dec1380765a9
Sedona Price <sedona.price@gmail.com>
2022-10-25 16:51:31 -0400

    Fix for >=10 comp namespace matching problem

--- galfit_parser.py
+++ galfit_parser.py
@@ -26,16 +26,16 @@ class GalfitComponent(object):
         headerkeys = [i for i in galfitheader.keys()]
         comp_params = []
         for i in headerkeys:
-            if str(component_number) + '_' in i:
+            if i.startswith(str(component_number)+'_'):
                 comp_params.append(i)
         for param in comp_params:
             val = galfitheader[param]
             #we know that val is a string formatted as 'result +/- uncertainty'
             #if val is fixed in GalFit, it is formatted as '[result]'
             paramsplit = param.split('_')
-            
+
             # If there's some numerical error, should output a warning (*)
-            
+
             if '[' in val:     #fixed parameter
                 val = val.translate(None,'[]*')
                 setattr(self,paramsplit[1].lower(),float(val))
@@ -44,7 +44,7 @@ class GalfitComponent(object):
                 val = val.translate(None,'*').split()
                 setattr(self,paramsplit[1].lower(),float(val[0]))
                 setattr(self,paramsplit[1].lower() + '_err',float(val[2]))
-            
+
 class GalfitResults(object):
     """
     This class stores galfit results information
@@ -67,7 +67,7 @@ class GalfitResults(object):
         assert True == galfit_in_comments
         assert "COMP_1" in galfitheader
         #now we've convinced ourselves that this is probably a galfit file
-        
+
         self.galfit_fits_file = galfit_fits_file
         #read in the input parameters
         self.input_initfile = galfitheader['INITFILE']
@@ -79,13 +79,13 @@ class GalfitResults(object):
         self.input_fitsect = galfitheader["FITSECT"]
         self.input_convbox = galfitheader["CONVBOX"]
         self.input_magzpt = galfitheader["MAGZPT"]
-        
+
         #read in the chi-square value
         self.chisq = galfitheader["CHISQ"]
         self.ndof = galfitheader["NDOF"]
         self.nfree = galfitheader["NFREE"]
         self.reduced_chisq = galfitheader["CHI2NU"]
-        
+
         #find the number of components
         num_components = 1 #already verified above
         while True:
@@ -99,4 +99,3 @@ class GalfitResults(object):
             setattr(self,"component_" + str(i),GalfitComponent(galfitheader,i))
 
         hdulist.close()
-        
